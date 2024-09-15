867b5d2d77692c40fb68be70772408ba8ed64e3f
Avinash Kumar Verma <avinashkumarverma2000@gmail.com>
2024-09-15 13:23:58 +0530

    update

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def load_user(user_id):
 # Index page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Hello, world!"  # Updated as per the minimal example
 
 # Login page
 @app.route('/login', methods=['GET', 'POST'])
